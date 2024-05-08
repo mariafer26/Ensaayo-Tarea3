3036475c7065c5a1dd5349722e40a8ac3a914e5e
Fernanda26 <mfalvarezm@eafit.edu.co>
2024-05-08 12:48:48 -0500

    hola linda

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ensaayo-Tarea3
---
+++ Tarea3.java
@@ -0,0 +1,12 @@
+public class Tarea3 {
+    /**
+     * Prints the phrase "Assignment 3 by <your full name>"
+     */
+    public static void printTarea() {
+        System.out.println("Assignment 3 by Maria Fernanda Alvarez Marin");
+    }
+
+    public static void main(String[] args) {
+        printTarea();
+    }
+}
